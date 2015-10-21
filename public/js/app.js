e684f475f64e0416bcf86b1c83f44426adf1e81a
Michael West <mwest@mdsol.com>
2015-10-21 14:58:02 -0400

    Clean up source a bit

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mpwest929.cdc89604',
-    accessToken: 'pk.eyJ1IjoibXB3ZXN0OTI5IiwiYSI6IjkzYzUxMTY4NTQ5NzEzOWY3NWU1MWY3YzNiMTUwMGIxIn0.j5Pp4ifPG1I4ZIMaGtKtqA'
+    accessToken: 'access-token'
 }).addTo(map);
 
 var recycleMarker = L.AwesomeMarkers.icon({
